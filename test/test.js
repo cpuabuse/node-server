9ee6fe68bda611a7fcfcce87d5fe4c3346f56100
foamy-latte <35882542+foamy-latte@users.noreply.github.com>
2019-01-25 00:00:04 +0900

    Add: Outline for tests

--- test/test.js
+++ test/test.js
@@ -32,6 +32,14 @@ describe("Server", function(){
 		}
 	];
 	var serverInstances = new Array();
+	var tests = [
+		{
+			name: "Bakery - About",
+			server: "localhost:81",
+			path: "/test/about",
+			status: 200
+		}
+	];
 
 	before(function(done){
 		var serverPromises = new Array();
@@ -63,10 +71,24 @@ describe("Server", function(){
 		});
 	});
 
-	it("should pass", function(){
-		chai.request('http://localhost:81').get('/test/about').end(function (err, res) {
-				console.log(res.text)
+	describe("Tests", function(){
+		tests.forEach(function(test){
+			describe(test.name, function(){
+				var response;
+				before(function(done){
+					chai.request(test.server).get(test.path).end(function (err, res){
+						if(err){
+							throw err;
+						}
+						response = res;
+						done();
+					});
+				});
+				it("should have status code " + test.status.toString(), function(){
+					response.should.have.status(test.status);
+				});
 			});
+		});
 	});
 
 	after(function(){
